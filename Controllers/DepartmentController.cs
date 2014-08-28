1dcc0a3d958c70fde224a5d20c0efaeceaa95ec6
pthakura <pthakura@gmail.com>
2014-08-28 06:44:30 +0530

    First Check in
    First Check in

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Mvc;
+using MySample.Model;
+using MySampleService.Proxy;
+namespace MySample.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        //
+        // GET: /Department/
+        public ActionResult Index()
+        {
+            return View("AddDepartment");
+        }
+
+        [HttpPost]
+        public ActionResult AddDepartment(Department model) {
+            if (ModelState.IsValid)
+            {
+                model.StartDate = DateTime.Now;
+                if (new DepartmentPort().SaveDepartment(model) > 0) {
+                    ViewBag.SuccessMessage = "Department details saved";
+                }
+            }
+            return View("AddDepartment");
+        }
+	}
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MySample.Startup))]
+namespace MySample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
